c2950be468fb6bc7ecaba75899e0c91284c680b1
Vijay <vp0013621@gmail.com>
2023-02-19 13:32:45 +0530

    round the logo corners at 19-02-2023 at 13:32

--- app/src/main/java/com/example/devsprofile/MainActivity.kt
+++ app/src/main/java/com/example/devsprofile/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.devsprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 //import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +18,10 @@ class MainActivity : AppCompatActivity() {
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 17f
+        logo.setImageDrawable(rounded)
+
     }
 }
